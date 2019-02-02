0b736f588b5c42938249e3491e11e58ef68084bb
Valverde Daniel <valverdedaniel44@gmail.com>
2019-02-02 11:46:16 -0500

    contractActions start

---
+++ client/src/actions/contractActions.js
@@ -0,0 +1,12 @@
+// Create Contract
+export const createContract = (contractData, history) => dispatch => {
+    axios
+      .post('/api/contract', contractData)
+      .then(res => history.push('/dashboard'))
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        })
+      );
+  };
